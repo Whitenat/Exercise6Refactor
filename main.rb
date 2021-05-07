52369896963903d484aae1827e51e6abc8eac3dc
Natashia White <whitenat@oregonstate.edu>
2021-05-07 16:30:30 -0700

    replace hard coded value with method call

--- main.rb
+++ main.rb
@@ -16,7 +16,7 @@ end
 def draw_button(label_text, x, y, foreground_color, is_dark_mode)
   if is_dark_mode
     # darken foreground color for dark mode
-    paint(label_text, x, y, calculate_foreground(foreground_color, is_dark_mode), '#111111')
+    paint(label_text, x, y, calculate_foreground(foreground_color, is_dark_mode), calculate_color(is_dark_mode))
   else
     # lighten foreground color for non-dark mode
     paint(label_text, x, y, calculate_foreground(foreground_color, is_dark_mode), '#E0E0E0')

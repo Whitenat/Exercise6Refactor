7c87d48d9f4f18d388c24e820aa4032443cdbb0d
Natashia White <whitenat@oregonstate.edu>
2021-05-07 16:16:00 -0700

    starting over

--- main.rb
+++ main.rb
@@ -1,11 +1,7 @@
-def foreground_color(foreground_color)
-	return foreground_color - 10
-end
-
 def draw_button(label_text, x, y, foreground_color, is_dark_mode)
   if is_dark_mode
     # darken foreground color for dark mode
-    paint(label_text, x, y, foreground_color(foreground_color), '#111111')
+    paint(label_text, x, y, foreground_color - 10, '#111111')
   else
     # lighten foreground color for non-dark mode
     paint(label_text, x, y, foreground_color + 10, '#E0E0E0')

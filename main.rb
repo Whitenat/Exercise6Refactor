4de7529346ff4768a2446b3720daf91294f5b1f7
Natashia White <whitenat@oregonstate.edu>
2021-05-09 17:49:41 -0700

    replace long param list with button object

--- main.rb
+++ main.rb
@@ -1,17 +1,17 @@
-def draw_dark_button(label_text, x, y, foreground_color)
-  paint(label_text, x, y, foreground_color - 10, '#111111')
+def draw_dark_button(button)
+  paint(button.label_text, button.x, button.y, button.foreground_color - 10, '#111111')
 end
 
-def draw_light_button(label_text, x, y, foreground_color)
-  paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
+def draw_light_button(button)
+  paint(button.label_text, button.x, button.y, button.foreground_color + 10, '#E0E0E0')
 end
 
-def draw_button(label_text, x, y, foreground_color, is_dark_mode)
+def draw_button(button, is_dark_mode)
   if is_dark_mode
     # darken foreground color for dark mode
-    draw_dark_button(label_text, x, y, foreground_color)
+    draw_dark_button(button)
   else
     # lighten foreground color for non-dark mode
-    draw_light_button(label_text, x, y, foreground_color)
+    draw_light_button(button)
   end
 end

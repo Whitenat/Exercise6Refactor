b3e30f3d0d12a352ecf43da60ab760ea88fcf33b
Natashia White <whitenat@oregonstate.edu>
2021-05-09 17:47:57 -0700

    replace call to paint with call to draw dark button

--- main.rb
+++ main.rb
@@ -9,7 +9,7 @@ end
 def draw_button(label_text, x, y, foreground_color, is_dark_mode)
   if is_dark_mode
     # darken foreground color for dark mode
-    paint(label_text, x, y, foreground_color - 10, '#111111')
+    draw_dark_button(label_text, x, y, foreground_color)
   else
     # lighten foreground color for non-dark mode
     draw_light_button(label_text, x, y, foreground_color)

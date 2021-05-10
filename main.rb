f75583d26febacd9548357a9ca112a301b6ceee7
Natashia White <whitenat@oregonstate.edu>
2021-05-09 17:37:54 -0700

    remove conditional and flag argument

--- main.rb
+++ main.rb
@@ -1,11 +1,5 @@
-def draw_dark_button(label_text, x, y, foreground_color, is_dark_mode = true)
-  if is_dark_mode
-    # darken foreground color for dark mode
-    paint(label_text, x, y, foreground_color - 10, '#111111')
-  else
-    # lighten foreground color for non-dark mode
-    paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
-  end
+def draw_dark_button(label_text, x, y, foreground_color)
+  paint(label_text, x, y, foreground_color - 10, '#111111')
 end
 
 def draw_button(label_text, x, y, foreground_color, is_dark_mode)
